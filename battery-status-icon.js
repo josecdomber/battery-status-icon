a09d844ca5bd599d87e07f3f3b57e0558df3b6c3
Dominguez Bermudez <jcdominguez@minsait.com>
2019-08-19 18:31:01 +0200

    first commit

---
+++ battery-status-icon.js
@@ -0,0 +1,9 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class BatteryStatusIcon extends LitElement {
+
+    render() {
+        return html``;
+    }
+}
+customElements.define('battery-status-icon', BatteryStatusIcon);
