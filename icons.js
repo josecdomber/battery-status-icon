477254185732d5ce894140e0e09e85468ce37511
Dominguez Bermudez <jcdominguez@minsait.com>
2019-08-21 23:23:24 +0200

    Set documentation

---
+++ icons.js
@@ -0,0 +1,52 @@
+import { html } from 'lit-element';
+
+export const icons = {
+    battery_20: html `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 17v3.67C7 21.4 7.6 22 8.33 22h7.33c.74 0 1.34-.6 1.34-1.33V17H7z"/><path fill-opacity=".3" d="M17 5.33C17 4.6 16.4 4 15.67 4H14V2h-4v2H8.33C7.6 4 7 4.6 7 5.33V17h10V5.33z"/></svg>
+    `,
+    battery_30: html `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill-opacity=".3" d="M17 5.33C17 4.6 16.4 4 15.67 4H14V2h-4v2H8.33C7.6 4 7 4.6 7 5.33V15h10V5.33z"/><path d="M7 15v5.67C7 21.4 7.6 22 8.33 22h7.33c.74 0 1.34-.6 1.34-1.33V15H7z"/></svg>
+    `,
+    battery_50: html `
+    
+    `,
+    battery_60: html `
+    
+    `,
+    battery_80: html `
+    
+    `,
+    battery_90: html `
+    
+    `,
+    battery_alert: html `
+    
+    `,
+    battery_charging_20: html `
+    
+    `,
+    battery_charging_30: html `
+    
+    `,
+    battery_charging_50: html `
+    
+    `,
+    battery_charging_60: html `
+    
+    `,
+    battery_charging_80: html `
+    
+    `,
+    battery_charging_90: html `
+    
+    `,
+    battery_charging_full: html `
+    
+    `,
+    battery_full: html `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.67 4H14V2h-4v2H8.33C7.6 4 7 4.6 7 5.33v15.33C7 21.4 7.6 22 8.33 22h7.33c.74 0 1.34-.6 1.34-1.33V5.33C17 4.6 16.4 4 15.67 4z"/></svg>
+    `,
+    battery_unknow: html `
+    
+    `,
+};
--- jcdb-battery-status-icon.js
+++ jcdb-battery-status-icon.js
@@ -1,9 +1,70 @@
 import { LitElement, html, css } from 'lit-element';
+import { icons } from './icons';
 
-export class BatteryStatusIcon extends LitElement {
+/**
+ * # Battery status icon
+ *
+ * <jcdb-battery-status-icon></jcdb-battery-status-icon>
+ *
+ * Web component to create battery level icons, with customized design. Based on LitElement.
+ *
+ * ## Styling
+ *
+ * ```
+ * <jcdb-battery-status-icon
+ *   icon="text to select the available icon"
+ * ></jcdb-battery-status-icon
+ * ```
+ *
+ * Custom property | Description | Default
+ * ----------------|-------------|---------
+ * --jcdb-battery-status-icon-size | Font size for the icon | 24px
+ * --jcdb-battery-status-icon-color | Color for the icon | #888
+ *
+ */
+
+export class JcdbBatteryStatusIcon extends LitElement {
+
+    static get properties() {
+        return {
+        /** Text to select the icon */
+            icon: { type: String }
+        };
+    }
+
+    constructor() {
+        super();
+        this.icon = 'battery_full'
+    }
+
+    static get styles() {
+        return css`
+          :host[hidden] { display: none; }
+          :host { display: inline-block; }
+          path {
+            fill: var(--jcdb-battery-status-icon-color, #888);
+          }
+          path[fill="none"] {
+            fill: transparent;
+          }
+          svg {
+            width: var(--jcdb-battery-status-icon-size, 24px);
+            height: var(--jcdb-battery-status-icon-size, 24px);
+          }
+          div { display: flex;}
+        `;
+      }
 
     render() {
-        return html``;
+        return html`
+        <div>
+            ${this._getIcon(this.icon)}
+        </div>
+        `;
+    }
+
+    _getIcon(icon) {
+        return icons[icon];
     }
 }
-customElements.define('battery-status-icon', BatteryStatusIcon);
+customElements.define('jcdb-battery-status-icon', JcdbBatteryStatusIcon);
